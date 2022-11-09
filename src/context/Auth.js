43723c6ecfe2043d9c628aee7aff5084ec86a97e
phong310 <phamdinhphong12@gmail.com>
2022-11-09 21:43:47 +0700

    source code

--- src/context/Auth.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const AuthContext = createContext({
-    user: null,
-    setUser: () => {}
-});
-
-export default AuthContext;
--- src/context/Cart.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const CartContext = createContext({
-  cartItem: [],
-  setCartItem: () => {},
-});
-
-export default CartContext;
