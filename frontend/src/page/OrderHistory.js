f317c4adc47d7262ddc21b6274fb25d8cef52412
phong310 <phamdinhphong12@gmail.com>
2023-04-14 22:24:02 +0700

    update

--- frontend/src/page/OrderHistory.js
+++ frontend/src/page/OrderHistory.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
-import { Col, Divider } from 'antd'
+import { Col, Divider, Card } from 'antd'
 import Footer from '../components/Footer'
+import axios from "axios"
 
 export default function OrderHistory() {
+    const [data, setData] = useState({})
+
+    const getOrderAll = async () => {
+        try {
+            const result = await axios.get("http://localhost:8000/order/getAllOrder");
+            setData(result.data)
+
+        } catch (e) {
+            console.log("Err", e)
+        }
+    }
+
+    useEffect(() => {
+        getOrderAll()
+    }, [])
+
+
     return (
         <>
             <Navbar />
-            <Col style={{ margin: "283px 0px" }}>
-                <h1>Chi tiết đơn hàng nhé</h1>
+            <Col style={{ margin: "100px 100px 290px 100px" }}>
+                <Card
+                    style={{
+                        width: 1200,
+                        margin: '0 auto'
+                    }}
+                    bordered={true}
+                >
+                    {JSON.stringify(data)}
+                </Card>
             </Col>
             <Divider />
             <Footer />
